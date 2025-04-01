7dbf442b34285ae2d35d876156062de3b6b8e8cd
Sumaiya Afrin <sumaiya.afrin1008@gmail.com>
2025-04-01 23:45:28 +0600

    fixed colors

--- src/components/Background.jsx
+++ src/components/Background.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Background = () => {
   return (
     <div className="fixed z-10 w-full h-screen">
-      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold">
+      <h1 className="absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%] text-[13vw] leading-none tracking-tighter font-semibold text-black">
         Docs.
       </h1>
     </div>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,13 +9,13 @@ const Card = ({ data, ref }) => {
       drag
       dragConstraints={ref}
       whileDrag={{ scale: 1.2 }}
-      className="relative w-60 h-72 rounded-3xl bg-amber-300 p-5 overflow-hidden"
+      className="relative w-60 h-72 rounded-3xl bg-amber-300 p-5 overflow-hidden text-black"
     >
       <FaRegFileAlt />
       <p className="text-sm leading-tight mt-5 font-semibold">{data.title}</p>
 
       <div className="footer absolute bottom-0  w-full left-0 ">
-        <div className="flex justify-between items-center mb-5 py-3 px-5">
+        <div className="flex justify-between w-full items-center mb-5 py-3 px-5">
           <h4>{data.storage}MB</h4>
           <span className="w-6 h-6 rounded-full bg-black text-white flex items-center justify-center text-sm">
             {data.downloadIcon ? <FiDownload /> : <IoCloseOutline />}
@@ -23,7 +23,7 @@ const Card = ({ data, ref }) => {
         </div>
 
         {!data.downloadIcon && (
-          <div className="w-full py-4 bg-amber-500 px-5">
+          <div className="w-full py-6 bg-amber-500 px-5">
             <progress
               className="progress !progress-warning h-1.5 w-full"
               value="40"
